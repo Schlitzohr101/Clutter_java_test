99df3b2601f5a001072a4cf21c09cc5687dbafef
Schlitzohr <wemx836@gmail.com>
2019-06-22 19:09:30 -0700

    in order to reduce complexity of the algorithm, a recursize formula was made. This insures the O(n) maximum complexity while still keeping functionality

--- main.java
+++ main.java
@@ -1,45 +1,56 @@
 import java.util.*;
-
 public class main {
     public static void main(String[] args) {
         List<String> phrases = new ArrayList<String>();  
         phrases.add("doesn't coding");
         phrases.add("where you find men");
-        phrases.add("coding rock");
+        phrases.add("coding rocks");
         phrases.add("you see men loving women");
         List<String> output = buildList(phrases);
         System.out.println(output);
     }
     private static List<String> buildList(List<String> phrases) {
         List<String> generatedList = new ArrayList<String>();
-        for (int i = 0; i < (phrases.size()/2) ; i++) {
-            String[] wordslist = phrases.get(i).split("\\s+");
-            String wordToFind = wordslist[wordslist.length-1];
-            System.out.println("word to find: "+ wordToFind);
-            String builder = "";
-            int current = phrases.size() - 1;
-            boolean found = false;
-            while (!found && current > i) {
-                System.out.println("word: " +wordToFind+ " searching: "+phrases.get(current));
-                String[] currentWords = phrases.get(current).split("\\s+");
-                boolean inList = false;
-                for (String word : currentWords) {
-                    System.out.println("word: "+word);
-                    if (inList) {
-                        found = true;
-                        builder += " " + word;
-                        System.out.println("current builder: "+builder);
-                    } else {
-                        System.out.println(inList = (word.contains(wordToFind)));
-                    }
-                }
-                current--;
-            }
+        
+        //get the first word to find
+        int indexOfWord = 0;
+        String wordToFind = phrases.get(indexOfWord).split("\\s+")[phrases.get(indexOfWord).split("\\s+").length-1];
+        int current = phrases.size() - 1;
+        // strCache.append(phrases.get(indexOfWord));
+        generatedList = buildListRec(phrases, generatedList, wordToFind, current, indexOfWord);
+
+
+        return generatedList;
+    }
+
+    private static List<String> buildListRec(List<String> phrases, List<String> genList,String wTf,int current,int index) {
+        StringBuilder strCache = new StringBuilder();
+        boolean found = false;
+        strCache.append(phrases.get(index));
+        System.out.println("word: " +wTf+ " searching: "+phrases.get(current));
+        strCache.append(phrases.get(current));
+        int srtIndex = strCache.indexOf(phrases.get(current).split("\\s+")[0]);
+        if (strCache.indexOf(wTf, srtIndex) > 0) {
+            System.out.println("word was found");
+            found = true;
+            //delete from the first word of the phrase untill the index of the reoccurence
+            strCache.delete(srtIndex, strCache.indexOf(wTf, srtIndex)+wTf.length());
+        } else {
+            System.out.println("word was not found");
+            strCache.delete(strCache.indexOf(phrases.get(current).split("\\s+")[0], srtIndex), strCache.length());
+        }
+        if (current > index) {
+            current--;
             if (found) {
-                System.out.println(phrases.get(i) + builder);
-                generatedList.add(phrases.get(i) + builder);
+                genList.add(strCache.toString());
+                index++;
+                current = phrases.size()-1;
+                wTf = phrases.get(index).split("\\s+")[phrases.get(index).split("\\s+").length-1];
+            } 
+            if (index < phrases.size()) {
+                buildListRec(phrases, genList, wTf, current, index);
             }
         }
-        return generatedList;
+        return genList;
     }
 }
